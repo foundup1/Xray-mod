40f7e4a0da714048d23b496eec8544ae3fbc3246
foundup1 <zmijul@gmail.com>
2026-02-22 00:20:44 +0300

    Create GameRendererMixin.java

---
+++ src/main/java/com/xraymod/client/mixin/GameRendererMixin.java
@@ -0,0 +1,26 @@
+package com.xraymod.client.mixin;
+
+import com.xraymod.client.XRayModClient;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockState.class)
+public class GameRendererMixin {
+
+    @Inject(method = "isOpaqueFullCube", at = @At("RETURN"), cancellable = true)
+    private void xray_isOpaqueFullCube(BlockRenderView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (XRayModClient.xRayManager.isEnabled()) {
+            BlockState state = (BlockState) (Object) this;
+            Block block = state.getBlock();
+            if (XRayModClient.xRayManager.shouldHideBlock(block)) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
